29d5350f4415a8257200ad3f77ca8d344bd4a37e
Brittney Oeur <oeur.brittney@gmail.com>
2024-12-12 22:20:24 -0800

    Created filter and sort button

--- lippie-finder/src/components/TopPart.jsx
+++ lippie-finder/src/components/TopPart.jsx
@@ -2,10 +2,11 @@ import ProductGrid from "./ProductGrid";
 import { useNavigate } from "react-router-dom";
 
 // Functional Component
-function Button({text, color, fontSize}){
+function Button({text, color, width, fontSize}){
     const buttonStyle = {
         color: color,
-        fontSize: fontSize + 'px'
+        fontSize: fontSize + 'px',
+        width: width
     };
 
     return (
@@ -33,9 +34,14 @@ function TopPart() {
                         type="text"
                         name="search" 
                         placeholder="Search lip product..." 
-                        style={{marginRight: "10px", padding: "5px", width: "220px"}} />
+                        style={{ marginRight: "10px", padding: "6px", width: "220px" }} />
 
-                <Button text="Search" color="pink" fontSize={12}/>
+                <Button text="Search" color="white" width="70px" fontSize={12} />
+            </div>
+
+            <div className="filterSort" style={{ display: "flex", padding: "15px", gap: "15px" }}>
+                <Button text="Filter" color="white"  width="100px" fontSize={12} />
+                <Button text="Sort" color="white"  width="100px" fontSize={12} />
             </div>
         </div>
     );
